e2b4e1e61c0dcb172718766d8d1ce561827be76b
YUPS Tech <yups.projects@gmail.com>
2016-05-03 18:54:48 +0530

    Update blink.py

--- blink.py
+++ blink.py
@@ -3,15 +3,15 @@ import time
 # blinking function  
 def blink(pin):  
   GPIO.output(pin,GPIO.HIGH)  
-  time.sleep(2)  
+  time.sleep(5)  
   GPIO.output(pin,GPIO.LOW)  
-  time.sleep(2)  
+  time.sleep(5)  
   return  
 # to use Raspberry Pi board pin numbers  
 GPIO.setmode(GPIO.BOARD)  
 # set up GPIO output channel  
 GPIO.setup(11, GPIO.OUT)  
 # blink GPIO17 50 times  
-for i in range(0,50):  
+for i in range(0,10):  
   blink(11)  
 GPIO.cleanup()   
